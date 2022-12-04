19d7838385d3784635fd3d1433c3a417ceb53744
vishal <vishalsinghh676@gmail.com>
2022-12-04 20:04:30 +0530

    haha

---
+++ src/test/java/Selenium/Test/acceptancetests/AllTests.java
@@ -0,0 +1,16 @@
+package Selenium.Test.acceptancetests;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = {"Selenium.Test"},
+        features = ".",
+        format = {"pretty", "json:target/cucumber-reports/cucumber.json"},
+        tags = {"@complete"}
+)
+public class AllTests {
+}
